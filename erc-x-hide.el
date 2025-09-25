2a541907a92fa171875bf5f647fe35ab81b2e411
Алькор Ф.З. Мкауед <7354509+fmqa@users.noreply.github.com>
2025-09-25 10:00:31 +0200

    Require erc-common

--- erc-x-hide.el
+++ erc-x-hide.el
@@ -36,7 +36,7 @@
 ;; override this.
 
 (require 'transient)
-(require 'erc)
+(require 'erc-common)
 
 (defvar erc-x-hide-list nil "Erc network-channel-message trie specifying message types to hide")
 
